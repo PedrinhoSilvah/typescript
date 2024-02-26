61d80d9395c671a61dce4ae24c05345dbdc14e27
Pedro Miguel <pedrinhosillvah59@gmail.com>
2024-02-26 01:21:41 -0300

    primeiro commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+function multiplicacao(soma1: number, somar2: number){
+   return soma1 + somar2;
+}
+
+console.log(multiplicacao(90, 20));
+
+const nome: string = "Pedro";
+
+function saudacao(){
+    return "Ola"+ nome;
+}
+console.log(saudacao());
